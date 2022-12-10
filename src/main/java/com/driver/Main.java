10f97afba28b4a4b6511e18e25f3e1a9e82bfc72
BHAVANI28122 <bhavanigavala@gmail.com>
2022-12-10 15:59:47 +0530

    do

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,26 +1,24 @@
 package com.driver;
-
+import java.util.Scanner;
 public class Main {
     public static class Product{
 
-        public  static int Product(int x, int y) {
+        public int product(int x, int y) {
             return x*y;
         }
-        public static  int Product(int x, int y,int z) {
+        public int product(int x, int y,int z) {
             return x*y*z;
         }
-        public  static double Product(double x, double y) {
-
-            return x*y;
-        }
+        public  double product(double x, double y) {return x*y;}
 
         }
 
     public static void main(String[] args){
+        Scanner Sc = new Scanner(System.in);
         Product p = new Product();
-        p.Product(3,5);
-        p.Product(3,2,6);
-        p.Product(3.2 ,4.5);
+        p.product(3,5);
+        p.product(3,2,6);
+        p.product(3.2 ,4.5);
 
     }
 
