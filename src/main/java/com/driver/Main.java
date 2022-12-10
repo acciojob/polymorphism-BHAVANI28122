0f87abd278dc3159ac3ba09c9f3a5e57099c7905
BHAVANI28122 <bhavanigavala@gmail.com>
2022-12-10 14:51:06 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
+    public static class product{
+
+        public  static int multiply(int x, int y) {
+
+            return x*y;
+        }
+        public static  int multiply(int x, int y,int z) {
+            return x*y*z;
+        }
+        public  static double multiply(double x, double y) {
+            return x*y;
+        }
+
+        }
+
+    public static void main(String[] args){
+        product p = new product();
+        p.multiply(3,5);
+        p.multiply(3,2,6);
+        p.multiply(3.2 ,4.5);
+
+    }
 
 }
