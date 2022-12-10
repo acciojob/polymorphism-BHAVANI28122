b72edc91da16e57bea8064c1ba69072b1bf501f5
BHAVANI28122 <bhavanigavala@gmail.com>
2022-12-10 15:07:27 +0530

    do this

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,15 +1,10 @@
 package com.driver;
 
 public class Main {
-    public static class product{
+    public static class Product{
 
-        public  static int multiply(int x, int y) {
-
-            return x*y;
-        }
-        public static  int multiply(int x, int y,int z) {
-            return x*y*z;
-        }
+        public  static int multiply(int x, int y) {return x*y;}
+        public static  int multiply(int x, int y,int z) {return x*y*z;}
         public  static double multiply(double x, double y) {
             return x*y;
         }
@@ -17,7 +12,7 @@ public class Main {
         }
 
     public static void main(String[] args){
-        product p = new product();
+        Product p = new Product();
         p.multiply(3,5);
         p.multiply(3,2,6);
         p.multiply(3.2 ,4.5);
